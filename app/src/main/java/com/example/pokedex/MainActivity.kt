4b2170fd03335597da89730f8271aff275a3a9e4
AndersonPS94 <dson_adn@hotmail.com>
2025-02-24 20:45:46 -0300

    atualizando projeto

---
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.pokedex
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.pokedex.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+    }
+}
